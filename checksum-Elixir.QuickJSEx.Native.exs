730c6518ed07248c501c45f3aa1a551c196f6598
Danila Poyarkov <dev@dannote.net>
2026-03-08 01:18:35 +0300

    Add checksum file for v0.3.0 precompiled NIFs

---
+++ checksum-Elixir.QuickJSEx.Native.exs
@@ -0,0 +1,9 @@
+%{
+  "libquickjs_ex_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:242ac250389c18a05d06496773d6e8d725916e6ae13a8fe026ea87e3018461ad",
+  "libquickjs_ex_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:61b8897ae3093676b190e022591e974f5cfd42c8b87dd659551d67abe26144ac",
+  "libquickjs_ex_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b89f578cd8b2e0ec6387213ef26b2e429f7cb0047e34cdc9243f6e18a8169bf8",
+  "libquickjs_ex_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b4834518b1f3b8123039d6f08c66bfa7f90749877739b97dba0012b57d6f3064",
+  "libquickjs_ex_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:d65fe4dc76b90653443471d4790e370491115241e602bb23bf8ad4e2c9d9db99",
+  "quickjs_ex_nif-v0.3.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:e97b3ede9a0646d3e821ad290c0eb680eb7b9a6a108180406a8a7967e0de1562",
+  "quickjs_ex_nif-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:2c1b17630214e2711caa5142f2267821d5003365f0f3c85c27b8ba953b63b38b",
+}
